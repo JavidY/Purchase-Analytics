ec67c829e578c440b8a73121191bc312138411b4
unknown <javid.yusifov@yahoo.com>
2019-06-30 16:18:57 +0200

    added sqlalchemy mysql connection

--- src/db_init.py
+++ src/db_init.py
@@ -1,33 +1,5 @@
-import mysql.connector
-from mysql.connector import errorcode
+from sqlalchemy import create_engine
 
 
-config = {
-    'user': 'pa_oltp',
-    'password': 'mypassword',
-    'host': '127.0.0.1',
-    'port': '3306',
-    'raise_on_warnings': True
-}
-
-
-def connect_db():
-    """A connect function returning mysql connection object.
-    Connection arguments(username,pass,host,.etc)
-    are passed inside the function"""
-    try:
-        return mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something wrong with your username or password")
-            raise err
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-            raise err
-        else:
-            print(err)
-            raise err
-
-
-cnx = connect_db()
-print("Success")
+engine = create_engine('mysql+pymysql://pa_oltp:mypassword@127.0.0.1/pa_oltp')
+engine.connect()
