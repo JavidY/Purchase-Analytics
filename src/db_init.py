23a2b683f55e0aa4218ecf4a8a126206263377d7
unknown <javid.yusifov@yahoo.com>
2019-06-30 17:07:34 +0200

    changes to circleci

--- src/db_init.py
+++ src/db_init.py
@@ -1,5 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Department(Base):
+    __tablename__ = 'departments'
+
+    department_id = Column(Integer, primary_key=True)
+    department = Column(String(255), unique=True)
 
 
 engine = create_engine('mysql+pymysql://pa_oltp:mypassword@127.0.0.1/pa_oltp')
-engine.connect()
+Base.metadata.create_all(engine)
