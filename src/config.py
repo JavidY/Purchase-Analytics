5ab59a558135d1f5b4472e4307e5d49ed6cbc705
unknown <javid.yusifov@yahoo.com>
2019-07-08 22:13:50 +0200

    changed config file to include root dir

--- src/config.py
+++ src/config.py
@@ -1,12 +1,17 @@
 import json
+import os
+
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+CONFIG_FILE = "{0}\\src\\{1}".format(ROOT_DIR, 'config.json')
 
 
 # retrieves path to input files from app directory
 def get_input_path():
-    with open('config.json') as json_data_file:
+    with open(CONFIG_FILE) as json_data_file:
         data = json.load(json_data_file)
-        return data.get('paths').get('source')
+        return "{0}\\{1}".format(ROOT_DIR, data.get('paths').get('source'))
 
 
 if __name__ == '__main__':
-    print(get_input_path())
+	print(get_input_path())
