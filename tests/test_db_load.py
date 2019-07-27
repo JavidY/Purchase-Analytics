284091c3a4864dffc98d87c6f38a1f1977f55b27
unknown <javid.yusifov@yahoo.com>
2019-07-27 22:57:20 +0200

    added function for reading csv file

--- tests/test_db_load.py
+++ tests/test_db_load.py
@@ -6,7 +6,7 @@ class TestDbLoad(unittest.TestCase):
 
     def test_read_csv(self):
         self.assertEqual(db_load.read_csv(
-            db_load.get_input_filenames('departments_test.csv')[0]),
+            db_load.get_input_filenames('departments.csv')[0]),
             [('1', 'frozen'), ('3', 'bakery')])
 
 
