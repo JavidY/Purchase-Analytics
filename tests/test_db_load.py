95443fae605a9e6919736efbf9d19144ccebd6ab
unknown <javid.yusifov@yahoo.com>
2019-08-18 13:19:01 +0200

    setup Travis CI

--- tests/test_db_load.py
+++ tests/test_db_load.py
@@ -7,7 +7,7 @@ class TestDbLoad(unittest.TestCase):
     def test_read_csv(self):
         self.assertEqual(db_load.read_csv(
             db_load.get_input_filenames('departments.csv')[0]),
-            [('1', 'frozen'), ('3', 'baakery')])
+            [('1', 'frozen'), ('3', 'bakery')])
 
 
 if __name__ == '__main__':
